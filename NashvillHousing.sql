93d6211fe0167cfe47ff08d717344476cc44404c
husseinch01 <79516682+husseinch01@users.noreply.github.com>
2023-06-21 10:20:22 +0300

    NashvillHousing.sql

--- NashvillHousing.sql
+++ NashvillHousing.sql
@@ -1,16 +1,7 @@
-/*
-
-Cleaning Data in SQL Queries
-
-*/
 
 
 Select *
 From [Portfolio Project].dbo.NashvilleHousing
-
---------------------------------------------------------------------------------------------------------------------------
-
--- Standardize Date Format
 
 
 Select saleDateConverted, CONVERT(Date,SaleDate)
@@ -20,7 +11,6 @@ From [Portfolio Project].dbo.NashvilleHousing
 Update NashvilleHousing
 SET SaleDate = CONVERT(Date,SaleDate)
 
--- If it doesn't Update properly
 
 ALTER TABLE NashvilleHousing
 Add SaleDateConverted Date;
@@ -28,10 +18,6 @@ Add SaleDateConverted Date;
 Update NashvilleHousing
 SET SaleDateConverted = CONVERT(Date,SaleDate)
 
-
- --------------------------------------------------------------------------------------------------------------------------
-
--- Populate Property Address data
 
 Select *
 From [Portfolio Project].dbo.NashvilleHousing
@@ -56,13 +42,7 @@ JOIN [Portfolio Project].dbo.NashvilleHousing b
 	AND a.[UniqueID ] <> b.[UniqueID ]
 Where a.PropertyAddress is null
 
-
-
-
---------------------------------------------------------------------------------------------------------------------------
-
--- Breaking out Address into Individual Columns (Address, City, State)
-
+	
 
 Select PropertyAddress
 From [Portfolio Project].dbo.NashvilleHousing
@@ -140,12 +120,6 @@ From [Portfolio Project].dbo.NashvilleHousing
 
 
 
---------------------------------------------------------------------------------------------------------------------------
-
-
--- Change Y and N to Yes and No in "Sold as Vacant" field
-
-
 Select Distinct(SoldAsVacant), Count(SoldAsVacant)
 From [Portfolio Project].dbo.NashvilleHousing
 Group by SoldAsVacant
@@ -155,11 +129,6 @@ order by 2
 
 
 Select SoldAsVacant
-, CASE When SoldAsVacant = 'Y' THEN 'Yes'
-	   When SoldAsVacant = 'N' THEN 'No'
-	   ELSE SoldAsVacant
-	   END
-From [Portfolio Project].dbo.NashvilleHousing
 
 
 Update NashvilleHousing
@@ -170,47 +139,7 @@ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
 
 
 
-
-
-
------------------------------------------------------------------------------------------------------------------------------------------------------------
-
--- Remove Duplicates
-
-WITH RowNumCTE AS(
-Select *,
-	ROW_NUMBER() OVER (
-	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY
-					UniqueID
-					) row_num
-
-From [Portfolio Project].dbo.NashvilleHousing
---order by ParcelID
-)
-Select *
-From RowNumCTE
-Where row_num > 1
-Order by PropertyAddress
-
-
-
-Select *
-From [Portfolio Project].dbo.NashvilleHousing
-
-
-
-
----------------------------------------------------------------------------------------------------------
-
--- Delete Unused Columns
-
-
-
+--unused columns
 Select *
 From [Portfolio Project].dbo.NashvilleHousing
 
@@ -225,62 +154,6 @@ DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
 
 
 
-
-
-
-
-
-
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
 
 
 
