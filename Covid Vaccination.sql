0e72c1abec3f1f004f69fa4288b22cd31bcd07cb
Hussein El Charif <79516682+HusseinElCharif@users.noreply.github.com>
2023-07-12 13:18:09 +0300

    Update Covid Vaccination.sql

--- Covid Vaccination.sql
+++ Covid Vaccination.sql
@@ -1,18 +1,18 @@
 
 SELECT *
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 3,4
 --Select the data that we are going to be using
 SELECT location,date,total_cases,new_cases,total_deaths,population
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 ORDER BY 1,2
 
 
 --looking at total cases vs total deaths
 --shows the percentage of dying if you attract covid in your country
 SELECT location,date,total_cases,total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE location LIKE '%lebanon%'
 ORDER BY location,date
 
@@ -21,7 +21,7 @@ ORDER BY location,date
 --looking at total cases vs population
 --shows percentage of population that got covid
 SELECT location,date,population,total_cases, (total_cases/population)*100 AS DeathPercentage
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE location LIKE '%lebanon%'
 ORDER BY location,date
 
@@ -29,7 +29,7 @@ ORDER BY location,date
 
 --looking at countries with highest infection rate compared to population
 SELECT location,population,MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentOfPopulationInfected
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 --WHERE location LIKE '%lebanon%'
 GROUP BY population,location
 ORDER BY PercentOfPopulationInfected DESC
@@ -38,7 +38,7 @@ ORDER BY PercentOfPopulationInfected DESC
 
 --showing countries with the highest death count per population
 SELECT location,MAX(CAST(total_deaths AS INT)) AS TotalDeathCount --cast to int if the data doesn't order it correctly
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY TotalDeathCount DESC
@@ -46,7 +46,7 @@ ORDER BY TotalDeathCount DESC
 
 --showing continents with highest death count
 SELECT location,MAX(CAST(total_deaths AS INT)) AS TotalDeathCount --cast to int if the data doesn't order it correctly
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NULL
 GROUP BY location
 ORDER BY TotalDeathCount DESC
@@ -54,7 +54,7 @@ ORDER BY TotalDeathCount DESC
 --this shows north america as all of america, because
 --of the null values in the excel sheet, so we use the query above
 SELECT continent,MAX(CAST(total_deaths AS INT)) AS TotalDeathCount --cast to int if the data doesn't order it correctly
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY continent
 ORDER BY TotalDeathCount DESC
@@ -63,7 +63,7 @@ ORDER BY TotalDeathCount DESC
 --Global Numbers
 SELECT SUM(new_cases) AS TotalCases, SUM(cast(new_deaths AS INT)) AS TotalDeaths, SUM(cast(new_deaths AS INT))/SUM
 (new_cases)*100 AS DearthPercentage
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 --GROUP BY date
 ORDER BY 1,2
@@ -71,15 +71,15 @@ ORDER BY 1,2
 --to view day by day
 SELECT date,SUM(new_cases) AS TotalCases, SUM(cast(new_deaths AS INT)) AS TotalDeaths, SUM(cast(new_deaths AS INT))/SUM
 (new_cases)*100 AS DearthPercentage
-FROM [Portfolio Project]..CovidDeaths
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
 
 
 SELECT *
-FROM [Portfolio Project]..CovidDeaths deaths
-JOIN [Portfolio Project]..CovidVaccinations vaccinations
+FROM CovidDeaths deaths
+JOIN CovidVaccinations vaccinations
 ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 
 --looking at total population vs vaccinations
@@ -87,8 +87,8 @@ ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 SELECT deaths.continent,deaths.location,deaths.date,deaths.population,vaccinations.new_vaccinations
  ,SUM(CONVERT(INT,vaccinations.new_vaccinations))  OVER(partition BY deaths.location ORDER BY deaths.location
  ,deaths.date) AS PeopleVaccinated
-FROM [Portfolio Project]..CovidDeaths deaths
-JOIN [Portfolio Project]..CovidVaccinations vaccinations
+FROM CovidDeaths deaths
+JOIN CovidVaccinations vaccinations
 ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 WHERE deaths.continent IS NOT NULL
 ORDER BY continent, location, date
@@ -102,8 +102,8 @@ AS
 SELECT deaths.continent,deaths.location,deaths.date,deaths.population,vaccinations.new_vaccinations
  ,SUM(CONVERT(INT,vaccinations.new_vaccinations))  OVER(partition BY deaths.location ORDER BY deaths.location
  ,deaths.date) AS PeopleVaccinated
-FROM [Portfolio Project]..CovidDeaths deaths
-JOIN [Portfolio Project]..CovidVaccinations vaccinations
+FROM CovidDeaths deaths
+JOIN CovidVaccinations vaccinations
 ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 WHERE deaths.continent IS NOT NULL
 --ORDER BY continent, location, date
@@ -126,8 +126,8 @@ INSERT INTO #PercentPopVac
 SELECT deaths.continent,deaths.location,deaths.date,deaths.population,vaccinations.new_vaccinations
  ,SUM(CONVERT(INT,vaccinations.new_vaccinations))  OVER(partition BY deaths.location ORDER BY deaths.location
  ,deaths.date) AS PeopleVaccinated
-FROM [Portfolio Project]..CovidDeaths deaths
-JOIN [Portfolio Project]..CovidVaccinations vaccinations
+FROM CovidDeaths deaths
+JOIN CovidVaccinations vaccinations
 ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 WHERE deaths.continent IS NOT NULL
 --ORDER BY continent, location, date
@@ -142,8 +142,8 @@ CREATE VIEW PercentPopVac AS
 SELECT deaths.continent,deaths.location,deaths.date,deaths.population,vaccinations.new_vaccinations
  ,SUM(CONVERT(INT,vaccinations.new_vaccinations))  OVER(partition BY deaths.location ORDER BY deaths.location
  ,deaths.date) AS PeopleVaccinated
-FROM [Portfolio Project]..CovidDeaths deaths
-JOIN [Portfolio Project]..CovidVaccinations vaccinations
+FROM CovidDeaths deaths
+JOIN CovidVaccinations vaccinations
 ON deaths.location=vaccinations.location AND deaths.date=vaccinations.date
 WHERE deaths.continent IS NOT NULL
 --ORDER BY continent, location, date
